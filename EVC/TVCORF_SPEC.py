ae32884b280602024671b12cab10cc1aa5c6da7c
sunxb05 <sunxb05@gmail.com>
2022-12-12 16:44:13 +0800

    doc

---
+++ EVC/TVCORF_SPEC.py
@@ -0,0 +1,36 @@
+"""Documentation about the EVC module."""
+
+
+def DUSHIN(logic):
+    """是否开启 Duschinsky 转动效应的计算
+
+    Args:
+        .t. (Default): .f. 
+
+    Example:
+        >>> &control
+        >>>     DUSHIN      = .t.        
+    """
+
+def HERZ(logic):
+    """是否开启 Herzberg-Teller 效应的计算
+
+    Args:
+        .f. (Default): .t. 
+
+    Example:
+        >>> &control
+        >>>     HERZ      = .f.        
+    """
+
+def Temp(float):
+    """ 定义温度
+
+    Args:
+        298 (Default)
+ 
+
+    Example:
+        >>> &control
+        >>>     Temp      = 298 K    
+    """
