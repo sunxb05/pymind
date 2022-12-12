1113d16f7a4f8bfc8a39393a03de464277375d95
sunxb05 <sunxb05@gmail.com>
2022-12-12 15:17:28 +0800

    doc

---
+++ MOMAP/TVCORF_SPEC.py
@@ -0,0 +1,16 @@
+"""Documentation about the TVCORF_SPEC module."""
+
+
+# FIXME: put actual code here
+def DUSHIN:
+    """是否开启 Duschinsky 转动效应的计算
+
+    Default: .t.
+
+    Example:
+        This function can be called with `Jane Smith` as argument using
+
+        &control
+          DUSHIN      = .t.        
+
+    """
