389f0823b371af9f4ffc98600d62f75b5f3be66f
sunxb05 <sunxb05@gmail.com>
2022-12-12 16:03:29 +0800

    doc

--- MOMAP/TVCORF_SPEC.py
+++ MOMAP/TVCORF_SPEC.py
@@ -13,3 +13,29 @@ def DUSHIN(logic):
             DUSHIN      = .t.        
 
     """
+
+def HERZ(logic):
+    """是否开启 Herzberg-Teller 效应的计算
+
+    Args:
+        .f. (Default): .t. 
+
+    Usage:
+
+        &control
+            HERZ      = .f.        
+
+    """
+
+def Temp(float K):
+    """ 定义温度
+
+    Args:
+        .298 K. (Default)
+
+    Usage:
+
+        &control
+            Temp      = 298 K       
+
+    """
