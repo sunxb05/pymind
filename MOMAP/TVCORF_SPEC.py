fbea33226ac810b2fb43caf0287c675a0b398dbd
sunxb05 <sunxb05@gmail.com>
2022-12-12 15:52:22 +0800

    doc

--- MOMAP/TVCORF_SPEC.py
+++ MOMAP/TVCORF_SPEC.py
@@ -5,13 +5,13 @@ def DUSHIN():
     """是否开启 Duschinsky 转动效应的计算
 
     Args:
-        .t. or .f. (logic)
+        .t. (logic): .f. 
 
     Default: .t.
 
     Example:
 
         &control
-        DUSHIN      = .t.        
+            DUSHIN      = .t.        
 
     """
