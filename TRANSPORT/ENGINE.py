2d61f4ae8c995ca0e84b864f6d4da0441e470f6f
sunxb05 <sunxb05@gmail.com>
2022-12-13 11:55:24 +0800

    transport

--- TRANSPORT/ENGINE.py
+++ TRANSPORT/ENGINE.py
@@ -11,9 +11,30 @@ def compute_engine(int):
         >>>     compute_engine      = 1      
     """
 
+def module_mpich2(str):
+    """MOMAP路径
+
+    Args:
+        momap (Default)
+
+    Example:
+        >>> &transport
+        >>>     module_mpich2 = momap      
+    """
+
+def module_qc(str):
+    """计算引擎路径
+
+    Args:
+        gaussian/g09.e01 (Default)
+
+    Example:
+        >>> &transport
+        >>>     module_qc = gaussian/g09.e01      
+    """
 
 def qc_exe(str):
-    """g09/g16 or fullpath/orca or qchem
+    """计算引擎可执行程序，g09/g16 or full path/orca or qchem or BDF
 
     Args:
         g09 (Default)
@@ -23,5 +44,91 @@ def qc_exe(str):
         >>>     qc_exe = g09      
     """
 
+def qc_method(str):
+    """计算引擎所用方法
+
+    Args:
+        b3lyp (Default)
+
+    Example:
+        >>> &transport
+        >>>     qc_method = b3lyp      
+    """
 
 
+def qc_basis(str):
+    """计算引擎所用基组
+
+    Args:
+        b3lyp cc-pvdz (Default)
+
+    Example:
+        >>> &transport
+        >>>     qc_basis = b3lyp cc-pvdz      
+    """
+
+def qc_basis_re(str):
+    """计算引擎计算重组能所用基组
+
+    Args:
+        b3lyp cc-pvdz (Default)
+
+    Example:
+        >>> &transport
+        >>>     qc_basis_re = b3lyp cc-pvdz      
+    """
+
+def qc_memory(str):
+    """计算引擎所用内存（in Mega）
+
+    Args:
+        4096 (Default)
+
+    Example:
+        >>> &transport
+        >>>     qc_memory = 4096      
+    """
+
+def qc_nodes(str):
+    """计算引擎申请使用节点数
+
+    Args:
+        1 (Default)
+
+    Example:
+        >>> &transport
+        >>>     qc_nodes = 1      
+    """
+
+def qc_ppn(str):
+    """计算引擎每节点并行运行核数
+
+    Args:
+        20 (Default)
+
+    Example:
+        >>> &transport
+        >>>     qc_ppn = 20      
+    """
+
+def queue_name(str):
+    """计算任务提交队列名
+
+    Args:
+        workq (Default)
+
+    Example:
+        >>> &transport
+        >>>     queue_name = workq      
+    """
+
+def sched_type(str):
+    """作业管理系统类别，pbs, slurm, lsf, or local
+
+    Args:
+        local (Default)
+
+    Example:
+        >>> &transport
+        >>>     sched_type = local      
+    """
--- TRANSPORT/TRANSPORT.py
+++ TRANSPORT/TRANSPORT.py
@@ -1,147 +1,272 @@
 """Documentation about the MOMAP module."""
 
 def do_transport_prepare(int):
-    """是否开启计算辐射关联函数, 1表示开启，0表示关闭
+    """是否生成预备文件, 1表示开启，0表示关闭
 
     Args:
         1 (Default): 0
 
     Example:
+        >>> &transport
         >>>     do_transport_prepare      = 1      
     """
 
 def do_transport_submit_HL_job(int):
-    """是否开启计算辐射光谱, 1表示开启，0表示关闭
+    """是否开启计算转移积分, 1表示开启，0表示关闭
 
     Args:
         1 (Default): 0
 
     Example:
+        >>> &transport
         >>>     do_transport_submit_HL_job      = 1      
     """
 
 def do_transport_get_transferintegral(int):
-    """是否开启计算内转换关联函数, 1表示开启，0表示关闭
+    """计算计算转移积分, 1表示开启，0表示关闭
 
     Args:
         1 (Default): 0
 
     Example:
+        >>> &transport
         >>>     do_transport_get_transferintegral      = 1      
     """
 
 def do_transport_submit_RE_job(int):
-    """是否开启计算内转换光谱, 1表示开启，0表示关闭
+    """计算重整能, 1表示开启，0表示关闭
 
     Args:
         1 (Default): 0
 
     Example:
+        >>> &transport
         >>>     do_transport_submit_RE_job      = 1      
     """
 
 
 def do_transport_get_re_evc(int):
-    """是否开启计算系间窜越关联函数, 1表示开启，0表示关闭
+    """使用 evc 程序分析重整能, 1表示开启，0表示关闭
 
     Args:
         1 (Default): 0
 
     Example:
+        >>> &transport
         >>>     do_transport_get_re_evc      = 1      
     """
 
 def do_transport_run_MC(int):
-    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+    """Monte_Carlo 模拟, 1表示开启，0表示关闭
 
     Args:
         1 (Default): 0
 
     Example:
+        >>> &transport
         >>>     do_transport_run_MC      = 1      
     """
 
 
 def do_transport_get_mob_MC(int):
-    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+    """计算迁移率, 1表示开启，0表示关闭
 
     Args:
         1 (Default): 0
 
     Example:
+        >>> &transport
         >>>     do_transport_get_mob_MC      = 1      
     """
 
 
 
 def do_transport_run_MC_temp(int):
-    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+    """不同温度下的Monte_Carlo 模拟, 1表示开启，0表示关闭
 
     Args:
-        1 (Default): 0
+        0 (Default): 1
 
     Example:
-        >>>     do_transport_run_MC_temp      = 1      
+        >>> &transport
+        >>>     do_transport_run_MC_temp      = 0      
     """
 
 def do_transport_get_mob_MC_temp(int):
-    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+    """计算不同温度下的迁移率, 1表示开启，0表示关闭
 
     Args:
-        1 (Default): 0
+        0 (Default): 1
 
     Example:
-        >>>     do_transport_get_mob_MC_temp      = 1      
+        >>> &transport
+        >>>     do_transport_get_mob_MC_temp      = 0      
     """
 
 def do_transport_run_ME(int):
-    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+    """ME 方面模拟, 1表示开启，0表示关闭
 
     Args:
         1 (Default): 0
 
     Example:
-        >>>     do_transport_run_ME      = 1      
+        >>> &transport
+        >>>     do_transport_run_ME      = 0   
     """
 
 
 def do_transport_get_mob_ME(int):
-    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+    """计算迁移率, 1表示开启，0表示关闭
 
     Args:
-        1 (Default): 0
+        0 (Default): 1
 
     Example:
-        >>>     do_transport_get_mob_ME      = 1      
+        >>> &transport
+        >>>     do_transport_get_mob_ME      = 0     
     """
 
 
 def do_transport_run_ME_temp(int):
-    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+    """不同温度下的 ME 模拟, 1表示开启，0表示关闭
 
     Args:
-        1 (Default): 0
+        0 (Default): 1
 
     Example:
-        >>>     do_transport_run_ME_temp      = 1      
+        >>> &transport
+        >>>     do_transport_run_ME_temp      = 0     
     """
 
 def do_transport_get_mob_ME_temp(int):
-    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+    """计算不同温度下的迁移率, 1表示开启，0表示关闭
 
     Args:
-        1 (Default): 0
+        0 (Default): 1
 
     Example:
-        >>>     do_transport_get_mob_ME_temp      = 1      
+        >>> &transport
+        >>>     do_transport_get_mob_ME_temp      = 0      
     """
 
 def do_transport_gather_momap_data(int):
-    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+    """收集打印电荷疏运计算的相关数据, 1表示开启，0表示关闭
 
     Args:
         1 (Default): 0
 
     Example:
+        >>> &transport
         >>>     do_transport_gather_momap_data      = 1      
+    """
+
+def crystal(str):
+    """定义晶体文件
+
+    Args:
+        crystal.cif (Default)
+
+    Example:
+        >>> &transport
+        >>>     crystal = naphthalene.cif      
+    """
+
+def lat_cutoff(float):
+    """定义晶胞截断距离
+
+    Args:
+        4 (Default)
+
+    Example:
+        >>> &transport
+        >>>     lat_cutoff = 4     
+    """
+
+def ratetype(str):
+    """定义电子空穴迁移速率计算方法，可选经典marcus方法或者量子修正的quantum方法
+
+    Args:
+        quantum (Default)：marcus
+
+    Example:
+        >>> &transport
+        >>>     ratetype = quantum     
+    """
+
+def temp(float):
+    """定义模拟温度
+
+    Args:
+        300 (Default)
+
+    Example:
+        >>> &transport
+        >>>     temp = 200     
+    """
+
+
+def start_temp(float):
+    """定义模拟初始温度
+
+    Args:
+        200 (Default)
+
+    Example:
+        >>> &transport
+        >>>     start_temp = 200     
+    """
+
+def end_temp(float):
+    """定义模拟最终温度
+
+    Args:
+        300 (Default)
+
+    Example:
+        >>> &transport
+        >>>     end_temp = 300     
+    """
+
+def delta_temp(float):
+    """定义模拟温度间隔
+
+    Args:
+        50 (Default)
+
+    Example:
+        >>> &transport
+        >>>     delta_temp = 50     
+    """
+
+def nsimu(int):
+    """定义模拟次数
+
+    Args:
+        2000 (Default)
+
+    Example:
+        >>> &transport
+        >>>     nsimu = 2000
+    """
+
+def tsimu(int):
+    """定义模拟时间（in ns）
+
+    Args:
+        1000 (Default)
+
+    Example:
+        >>> &transport
+        >>>     tsimu = 1000
+    """
+
+def tsnap(int):
+    """定义模拟取样间隔
+
+    Args:
+        5 (Default)
+
+    Example:
+        >>> &transport
+        >>>     tsnap = 5
     """
