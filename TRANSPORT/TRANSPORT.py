6f003ca4910bf8aa13a7285a3188c9b87cb2870c
sunxb05 <sunxb05@gmail.com>
2022-12-13 10:15:29 +0800

    doc

---
+++ TRANSPORT/TRANSPORT.py
@@ -0,0 +1,341 @@
+"""Documentation about the MOMAP module."""
+
+def do_transport_prepare(int):
+    """是否开启计算辐射关联函数, 1表示开启，0表示关闭
+
+    Args:
+        1 (Default): 0
+
+    Example:
+        >>>     do_transport_prepare      = 1      
+    """
+
+def do_transport_submit_HL_job(int):
+    """是否开启计算辐射光谱, 1表示开启，0表示关闭
+
+    Args:
+        1 (Default): 0
+
+    Example:
+        >>>     do_transport_submit_HL_job      = 1      
+    """
+
+def do_transport_get_transferintegral(int):
+    """是否开启计算内转换关联函数, 1表示开启，0表示关闭
+
+    Args:
+        1 (Default): 0
+
+    Example:
+        >>>     do_transport_get_transferintegral      = 1      
+    """
+
+def do_transport_submit_RE_job(int):
+    """是否开启计算内转换光谱, 1表示开启，0表示关闭
+
+    Args:
+        1 (Default): 0
+
+    Example:
+        >>>     do_transport_submit_RE_job      = 1      
+    """
+
+
+def do_transport_get_re_evc(int):
+    """是否开启计算系间窜越关联函数, 1表示开启，0表示关闭
+
+    Args:
+        1 (Default): 0
+
+    Example:
+        >>>     do_transport_get_re_evc      = 1      
+    """
+
+def do_transport_run_MC(int):
+    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+
+    Args:
+        1 (Default): 0
+
+    Example:
+        >>>     do_transport_run_MC      = 1      
+    """
+
+
+def do_transport_get_mob_MC(int):
+    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+
+    Args:
+        1 (Default): 0
+
+    Example:
+        >>>     do_transport_get_mob_MC      = 1      
+    """
+
+
+
+def do_transport_run_MC_temp(int):
+    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+
+    Args:
+        1 (Default): 0
+
+    Example:
+        >>>     do_transport_run_MC_temp      = 1      
+    """
+
+def do_transport_get_mob_MC_temp(int):
+    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+
+    Args:
+        1 (Default): 0
+
+    Example:
+        >>>     do_transport_get_mob_MC_temp      = 1      
+    """
+
+def do_transport_run_ME(int):
+    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+
+    Args:
+        1 (Default): 0
+
+    Example:
+        >>>     do_transport_run_ME      = 1      
+    """
+
+
+def do_transport_get_mob_ME(int):
+    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+
+    Args:
+        1 (Default): 0
+
+    Example:
+        >>>     do_transport_get_mob_ME      = 1      
+    """
+
+
+def do_transport_run_ME_temp(int):
+    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+
+    Args:
+        1 (Default): 0
+
+    Example:
+        >>>     do_transport_run_ME_temp      = 1      
+    """
+
+def do_transport_get_mob_ME_temp(int):
+    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+
+    Args:
+        1 (Default): 0
+
+    Example:
+        >>>     do_transport_get_mob_ME_temp      = 1      
+    """
+
+def do_transport_gather_momap_data(int):
+    """是否开启计算系间窜越光谱, 1表示开启，0表示关闭
+
+    Args:
+        1 (Default): 0
+
+    Example:
+        >>>     do_transport_gather_momap_data      = 1      
+    """
+
+
+def Ead(float):
+    """ 绝热激发能，可由Gaussian等软件计算得到
+
+    Args:
+        0.05 (Default)
+ 
+
+    Example:
+        >>> &spec_tvcf
+        >>>     Ead      = 0.075091878 au    
+    """
+
+def EDMA(float):
+    """ 吸收跃迁偶极矩，可由Gaussian等软件计算得到
+
+    Args:
+        0.05 (Default)
+ 
+
+    Example:
+        >>> &spec_tvcf
+        >>>     EDMA      = 0.92694 debye   
+    """
+
+def EDME(float):
+    """ 发射跃迁偶极矩，可由Gaussian等软件计算得到
+
+    Args:
+        0.05 (Default)
+ 
+
+    Example:
+        >>> &spec_tvcf
+        >>>     EDME      = 0.64751 debye   
+    """
+
+def Hso(float):
+    """ 旋轨耦合常数，可由Dalton等软件计算得到，用于系间窜越计算
+
+    Args:
+        100 (Default)
+ 
+
+    Example:
+        >>> &isc_tvcf
+        >>>     Hso      = 116.877376 cm-1   
+    """
+
+
+def Temp(float):
+    """ 定义温度
+
+    Args:
+        300 (Default)
+ 
+
+    Example:
+        >>> &spec_tvcf
+        >>>     Temp      = 300 K    
+    """
+
+def tmax(float):
+    """ 定义积分时间
+
+    Args:
+        1000 (Default)
+ 
+
+    Example:
+        >>> &spec_tvcf
+        >>>     tmax      = 1000 fs    
+    """
+
+def dt(float):
+    """ 定义积分步长
+
+    Args:
+        0.01 (Default)
+ 
+
+    Example:
+        >>> &spec_tvcf
+        >>>     dt      = 0.01 fs    
+    """
+
+
+def Emax(float):
+    """ 定义光谱频率范围上限
+
+    Args:
+        0.3 (Default)
+ 
+
+    Example:
+        >>> &spec_tvcf
+        >>>     Emax      = 0.3 au  
+    """
+
+def dE(float):
+    """ 定义输出能量间隔
+
+    Args:
+        0.00001 (Default)
+ 
+
+    Example:
+        >>> &spec_tvcf
+        >>>     dE      = 0.00001 au   
+    """
+
+def DSFile(str):
+    """定义读取的 evc 文件名
+
+    Args:
+        "evc.cart.dat" (Default)
+
+    Example:
+        >>> &spec_tvcf
+        >>>     DSFile = "evc.cart.dat"      
+    """
+
+def CoulFile(str):
+    """定义读取的nacme文件名，
+
+    Args:
+        "evc.cart.nac" (Default)
+
+    Example:
+        >>> &ic_tvcf
+        >>>     CoulFile = "evc.cart.nac"      
+    """
+
+def DDplFile(str):
+    """定义读取的 dip 文件名,用于Herzberg-Teller效应计算
+
+    Args:
+        "evc.cart.dip" (Default)
+
+    Example:
+        >>> &spec_tvcf
+        >>>     evc.cart.dip = "evc.cart.dip"      
+    """
+
+def logFile(str):
+    """定义输出 log 文件名
+
+    Args:
+        "spec.tvcf.log" (Default)
+
+    Example:
+        >>> &spec_tvcf
+        >>>     logFile = "spec.tvcf.log"      
+    """
+
+
+def FtFile(str):
+    """定义输出的关联函数文件名
+
+    Args:
+        "spec.tvcf.ft.dat" (Default)
+
+    Example:
+        >>> &spec_tvcf
+        >>>     FtFile = "spec.tvcf.ft.dat"      
+    """
+
+
+def FoFile(str):
+    """定义输出的谱函数输出文件名
+
+    Args:
+        "spec.tvcf.fo.dat" (Default)
+
+    Example:
+        >>> &spec_tvcf
+        >>>     FoFile = "spec.tvcf.fo.dat"      
+    """
+
+
+def FoSFile(str):
+    """定义输出的归一化的光谱输出文件名
+
+    Args:
+        "spec.tvcf.spec.dat" (Default)
+
+    Example:
+        >>> &spec_tvcf
+        >>>     FoSFile = "spec.tvcf.spec.dat"      
+    """
+
+
+
